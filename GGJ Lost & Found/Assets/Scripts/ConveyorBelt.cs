b85be7f70df6f44a060436b1e90e6ed14b18cca3
SplitFace <masi.edoardo@libero.it>
2021-01-30 00:15:30 +0100

    Added ConveyorBelt script

---
+++ GGJ Lost & Found/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody beltBody;
+
+    private void Start()
+    {
+        beltBody = GetComponent<Rigidbody>();
+        beltBody.isKinematic = true;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 position = beltBody.position;
+        beltBody.position += -transform.forward * speed * Time.fixedDeltaTime;
+        beltBody.MovePosition(position);
+    }
+}
