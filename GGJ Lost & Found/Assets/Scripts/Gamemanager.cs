7431685f5a0724164f71c9862f4db058e4cbb26c
SplitFace <masi.edoardo@libero.it>
2021-01-31 00:11:35 +0100

    Added Explosive Luggage prefab

--- GGJ Lost & Found/Assets/Scripts/Gamemanager.cs
+++ GGJ Lost & Found/Assets/Scripts/Gamemanager.cs
@@ -47,7 +47,6 @@ public class Gamemanager : Singleton<Gamemanager>
     {
         if (Player != null)
         {
-            Debug.Log("Onsceneloaded");
             StartNewGame();
         }
     }
