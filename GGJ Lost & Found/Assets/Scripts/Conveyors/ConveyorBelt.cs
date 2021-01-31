439b91264c2d39174b622f96bde578880ee67ec4
michele bonapace <michele.bonapace@gmail.com>
2021-01-31 10:44:38 +0100

    player new

--- GGJ Lost & Found/Assets/Scripts/Conveyors/ConveyorBelt.cs
+++ GGJ Lost & Found/Assets/Scripts/Conveyors/ConveyorBelt.cs
@@ -8,17 +8,23 @@ public class ConveyorBelt : MonoBehaviour
     public float speed;
     private Rigidbody beltBody;
 
-    private void Start()
+    private void Awake()
     {
-        speed = 2;
+        speed = 1;
         beltBody = GetComponent<Rigidbody>();
         beltBody.isKinematic = true;
+        beltBody.useGravity = false;
     }
 
+    private void Start()
+    {
+    }
+    
     private void FixedUpdate()
     {
         Vector3 position = beltBody.position;
         beltBody.position += -transform.right * speed * Time.fixedDeltaTime;
         beltBody.MovePosition(position);
     }
+    
 }
