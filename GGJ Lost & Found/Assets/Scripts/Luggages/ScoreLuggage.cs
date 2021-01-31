8d28032a3cca995086e16225f2904a30a2a01e1a
Giacomo Vannucci <jack.vannu@gmail.com>
2021-01-31 12:12:36 +0100

    score luggage script

---
+++ GGJ Lost & Found/Assets/Scripts/Luggages/ScoreLuggage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreLuggage : MonoBehaviour{
+
+    public LayerMask playerLayer;
+    public string playerTag;
+    public int score;
+
+    private void OnCollisionEnter(Collision collision) {
+
+        if ((playerLayer.value & 1 << collision.gameObject.layer) != 0 || collision.gameObject.tag.CompareTo(playerTag) == 0) {
+            Gamemanager.Instance.AddScore(score);
+            //TODO: destroy o boh
+        }
+    }
+}
